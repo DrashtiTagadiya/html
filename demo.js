67443bbc6bf5320fa9569c8be84d5d6a258877d6
DrashtiTagadiya <108564394+DrashtiTagadiya@users.noreply.github.com>
2022-07-08 09:53:16 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,16 @@
+function demoExternalAlert(){
+    alert("External Alert.");
+}
+
+function demoExternalConfirm(){
+    if(confirm("Are you sure...?")){
+        alert("YESSS");
+    }else{
+        alert("NOOO");
+    }
+}
+
+function demoExternalPrompt(){
+    var fName=prompt("Enter Firstname Here...");
+    var lName=prompt("Enter Lastname Here...");
+}
